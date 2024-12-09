999794f515c2680c02b4f009bb148da3ea59d112
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-12-09 15:01:05 +0100

    API setup

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+// routes.js
+const express = require('express');
+const AppController = require('../controllers/AppController');
+
+const router = express.Router();
+
+// Define routes
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+module.exports = router;
+
+
